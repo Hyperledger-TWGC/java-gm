4c3bd29a172c0cddbfa35c2c4548d1c9fabd5f58
sean <xiaohui_1123@126.com>
2020-09-18 16:01:37 +0800

    增加国密算法SM3和SM4的工具类
    Signed-off-by: xiaohui249 <xiaohui_1123@126.com>

---
+++ src/main/java/SM3Util.java
@@ -0,0 +1,47 @@
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+import java.util.Arrays;
+
+/**
+ * @author Sean
+ * @Description: 国密SM3工具类
+ * @date 2020/9/18
+ */
+public class SM3Util {
+
+    static{
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * 计算SM3摘要值
+     *
+     * @param srcData 原文
+     * @return 摘要值
+     */
+    public static byte[] hash(byte[] srcData) {
+        SM3Digest digest = new SM3Digest();
+        digest.update(srcData, 0, srcData.length);
+        byte[] hashVal = new byte[digest.getDigestSize()];
+        digest.doFinal(hashVal, 0);
+        return hashVal;
+    }
+
+    /**
+     * SM3摘要值验证
+     *
+     * @param srcData 原文
+     * @param sm3HashVal 原文对应的摘要值
+     * @return 返回true标识验证成功，false标识验证失败
+     */
+    public static boolean verify(byte[] srcData, byte[] sm3HashVal) {
+        byte[] hashVal = hash(srcData);
+        if (Arrays.equals(hashVal, sm3HashVal)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
