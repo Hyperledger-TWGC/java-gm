ee506a37cefa260fcad23153e4e86ccb84bc6a3d
davidliu <david-khala@hotmail.com>
2020-11-27 20:04:51 +0800

    [TEST] AZP debug
    Signed-off-by: davidliu <david-khala@hotmail.com>

--- releaseJar.sh
+++ releaseJar.sh
@@ -2,7 +2,7 @@
 TAGPATTERN="refs/tags/v.*"
 echo $1
 if [[ "$1" =~ $TAGPATTERN ]]; then
-  gradle publish -Dtoken=$2 -Drepo=$3 -Dusername=$4
+  gradle publish
 else
 	echo "skip as not relase tag!"
 fi
