31e6990bc899789255c4f8c6548c7b86b745bfd3
xiaohui249 <xiaohui_1123@126.com>
2021-02-15 19:55:11 +0800

    SM3算法使用对象通用池来管理Digest对象，优化性能同时避免线程安全
    Signed-off-by: Xiao Hui <xiaohui_1123@126.com>

--- src/main/java/twgc/gm/sm3/SM3Util.java
+++ src/main/java/twgc/gm/sm3/SM3Util.java
@@ -15,20 +15,17 @@ public class SM3Util {
 
     }
 
-    private static final SM3Digest digest = new SM3Digest();
     /**
      * 计算SM3摘要值
      *
      * @param srcData 原文
      * @return 摘要值
      */
-    public static byte[] hash(byte[] srcData) {
-        synchronized (SM3Util.class) {
-            digest.update(srcData, 0, srcData.length);
-            byte[] hashVal = new byte[digest.getDigestSize()];
-            digest.doFinal(hashVal, 0);
-            return hashVal;
-        }
+    public static byte[] hash(SM3Digest digest, byte[] srcData) {
+        digest.update(srcData, 0, srcData.length);
+        byte[] hashVal = new byte[digest.getDigestSize()];
+        digest.doFinal(hashVal, 0);
+        return hashVal;
     }
 
     /**
@@ -38,10 +35,8 @@ public class SM3Util {
      * @param sm3HashVal 原文对应的摘要值
      * @return 返回true标识验证成功，false标识验证失败
      */
-    public static boolean verify(byte[] srcData, byte[] sm3HashVal) {
-        synchronized (SM3Util.class) {
-            byte[] hashVal = hash(srcData);
-            return Arrays.equals(hashVal, sm3HashVal);
-        }
+    public static boolean verify(SM3Digest digest, byte[] srcData, byte[] sm3HashVal) {
+        byte[] hashVal = hash(digest, srcData);
+        return Arrays.equals(hashVal, sm3HashVal);
     }
 }
