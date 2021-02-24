b78f96fde82e333f8cbadf5622f3b9e922d4d3f5
xiaohui249 <xiaohui_1123@126.com>
2021-02-24 22:13:24 +0800

    SM4增加CFB、OFB、CTR三种模式，这三种模式都无需填充(padding)
    Signed-off-by: Xiao Hui <xiaohui_1123@126.com>

--- src/main/java/twgc/gm/sm4/SM4ModeAndPaddingEnum.java
+++ src/main/java/twgc/gm/sm4/SM4ModeAndPaddingEnum.java
@@ -11,7 +11,11 @@ public enum SM4ModeAndPaddingEnum {
     SM4_ECB_PKCS7Padding("SM4/ECB/PKCS7Padding"),
     SM4_CBC_NoPadding("SM4/CBC/NoPadding"),
     SM4_CBC_PKCS5Padding("SM4/CBC/PKCS5Padding"),
-    SM4_CBC_PKCS7Padding("SM4/CBC/PKCS7Padding");
+    SM4_CBC_PKCS7Padding("SM4/CBC/PKCS7Padding"),
+    // CFB,OFB,CTR三种模式无需填充(padding)
+    SM4_CFB_NoPadding("SM4/CFB/NoPadding"),
+    SM4_OFB_NoPadding("SM4/OFB/NoPadding"),
+    SM4_CTR_NoPadding("SM4/CTR/NoPadding");
 
     private final String name;
 
