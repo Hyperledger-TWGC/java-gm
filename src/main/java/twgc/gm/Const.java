4ab5be75719c3dd2802d4262bd06ff36ed79e372
Sam Yuan <yy19902439@126.com>
2021-07-19 22:45:49 +0800

    Repack cert2 (#61)
    * 增加cert模块，可颁发SM2证书
    
    Signed-off-by: EdgarLQS <751232541@qq.com>
    
    * refactor cert part code
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    * fix up
    
    Signed-off-by: Sam Yuan <yy19902439@126.com>
    
    Co-authored-by: EdgarLQS <751232541@qq.com>

---
+++ src/main/java/twgc/gm/Const.java
@@ -0,0 +1,11 @@
+package twgc.gm;
+
+public class Const {
+    private Const() {
+
+    }
+
+    public static final String EC_VALUE = "EC";
+    public static final String SM3SM2_VALUE = "SM3WITHSM2";
+    public static final String CURVE_NAME = "sm2p256v1";
+}
